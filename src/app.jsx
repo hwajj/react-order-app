c65b7fb9ba3d88a2d16fbb3f49261f917d8bac4a
hwajj <azamwara@gmail.com>
2022-10-19 17:41:58 +0900

    Add google login function

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,46 @@
 import './App.css';
-import { useState, useCallback } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Items from './components/Menu/Items';
 import Header from './components/UI/Header';
 import { addToCart, removeFromCart, getCartTotal } from './modules/cart';
 import ItemsContainer from './containers/ItemsContainer';
 import Cart from './components/Cart/Cart';
+import jwt_decode from 'jwt-decode';
 
 function App() {
+  //구글 로그인
+  const [user, setUser] = useState({});
+  function handleCallbackResponse(response) {
+    console.log('Encoded JWT ID token : ' + response.credential);
+    let userObject = jwt_decode(response.credential);
+    console.log(userObject);
+    setUser(userObject);
+    document.getElementById('signInDiv').hidden = true;
+  }
+  function handleSignOut(event) {
+    setUser({});
+    document.getElementById('signInDiv').hidden = false;
+  }
+  useEffect(() => {
+    /* global google */
+
+    google.accounts.id.initialize({
+      client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+      callback: handleCallbackResponse,
+    });
+
+    google.accounts.id.renderButton(document.getElementById('signInDiv'), {
+      theme: 'outline',
+      size: 'large',
+    });
+
+    google.accounts.id.prompt();
+  }, []);
+
+  //user 없음 -> 로그인버튼
+  //user 있음 -> 로그아웃버튼
+
   //증가 감소
   const items = useSelector((state) => state.cart.items);
   const dispatch = useDispatch();
@@ -44,7 +77,18 @@ function App() {
           onIncrease={onIncrease}
           onDecrease={onDecrease}
           total={getCartTotal}
+          userName={user.name}
         />
+      )}
+      <div id='signInDiv' />
+      {Object.keys(user).length != 0 && (
+        <button onClick={(e) => handleSignOut(e)}>로그아웃</button>
+      )}
+      {user && (
+        <div>
+          <img src={user.picture} alt='' srcset='' />
+          <h3>{user.name}</h3>
+        </div>
       )}
       <Header
         onShowCart={showCartHandler}
