beceb86a5bbe1c26b877cfea98a72679a3fc22bf
hwajj <azamwara@gmail.com>
2022-10-23 18:32:17 +0900

    Feat : 주소정보 redux에 추가

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,11 +1,11 @@
 //여러개의 리듀서 통합
 import { combineReducers } from 'redux';
 import cart from './cart';
-import googleReducer from './login';
+import userReducer from './login';
 
 const rootReducer = combineReducers({
   cart,
-  login: googleReducer,
+  login: userReducer,
 });
 
 export default rootReducer;
--- src/modules/login.js
+++ src/modules/login.js
@@ -1,6 +1,7 @@
 //액션 생성
 const LOGIN = 'login/USER_LOGIN';
 const LOGOUT = 'login/USER_LOGOUT';
+const ADD_ADDRESS = 'login/USER_ADDRESS';
 //액션생성함수
 export const logIn = (user) => {
   console.log(user);
@@ -11,16 +12,19 @@ export const logOut = () => {
   return { type: LOGOUT };
 };
 
+export const addAddress = (address) => {
+  return { type: ADD_ADDRESS, address };
+};
+
 const initialState = {};
 
-const googleReducer = (state = initialState, action) => {
+const userReducer = (state = initialState, action) => {
   console.log(action);
+  console.log(state);
   switch (action.type) {
     case LOGIN: {
       const user = action.user;
-      console.log(action);
-      console.log(state);
-      console.log(user);
+
       return { ...state, user };
     }
     case LOGOUT: {
@@ -28,10 +32,17 @@ const googleReducer = (state = initialState, action) => {
       const emptyUser = {};
       return { ...state, user: {} };
     }
+    case ADD_ADDRESS: {
+      const user = state.user;
 
+      const address = action.address;
+
+      console.log(user);
+      return { ...state, user: { ...user, address: action.address } };
+    }
     default:
       return state;
   }
 };
 
-export default googleReducer;
+export default userReducer;
